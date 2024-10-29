b05beefdceee66bd33e165dd8bcba30bfd47a7f4
DaqCode <dlim0012.gaming@gmail.com>
2024-10-29 14:16:20 -0700

    Animation not complete, HORRAY
    Need to replace some images or redraw them back. these are just placeholders

--- scripts/eat_a_burger_sponser.gd
+++ scripts/eat_a_burger_sponser.gd
@@ -3,4 +3,8 @@ extends Control
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
-	print("Ready")
+	animation_player.play("reveal")
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu2.tscn")
