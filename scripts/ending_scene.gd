4e8768d0bf182609f1908578e082dc6617d8e021
DaqCode <dlim0012.gaming@gmail.com>
2024-10-28 22:04:10 -0700

    How

--- scripts/ending_scene.gd
+++ scripts/ending_scene.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var byebye: AnimationPlayer = $dissapearAnimation
 
 func _on_ending_animation_finished() -> void:
-    byebye.play("dissapear")
-    await byebye.animation_finished
-    print("Bye bye played")
-    get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	byebye.play("dissapear")
+	await byebye.animation_finished
+	print("Bye bye played")
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
