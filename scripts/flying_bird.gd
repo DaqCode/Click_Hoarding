cb264edd046ef5aa044b4e777f56abbe426434c5
Daq_Vid <dlim0012.gaming@gmail.com>
2024-11-08 12:14:17 -0800

    Began to work on birds

--- scripts/flying_bird.gd
+++ scripts/flying_bird.gd
@@ -1,10 +1,46 @@
 extends Button
-@onready var flying_bird = $"."
+@onready var flying_bird: Button = $"."
+@onready var flying_timer: Timer = $"../Timers/FlyingTimer"
+@onready var flap_timer: Timer = $"../Timers/FlapTimer"
 
 var xPos = position.x 
 var yPos = position.y
+var canFly = false
 
-	
+const BIRD_ONE = preload("res://images/birb/birdOne.png")
+const BIRD_TWO = preload("res://images/birb/birdTwo.png")
+
+const BIRB_1 = preload("res://images/birb/birb1.png")
+const BIRB_2 = preload("res://images/birb/birb2.png")
+
+func _ready()-> void:
+	flying_timer.wait_time = randf_range(4,10)
+	print(flying_timer.wait_time)
+	flying_timer.start()
+
+	flap_timer.wait_time = randf_range(0.1,0.4)
+	flap_timer.start()
+		
 func _process(delta) -> void:
-	flying_bird.position.x += 100 * delta
+
+	if canFly == true:
+		flying_bird.position.x += 175 * delta
+		if flying_bird.position.x > 1500:
+			flying_bird.position.x = -200
+			canFly = false
+			flying_timer.wait_time = randf_range(4,10)
+			flying_timer.start()
 	
+
+
+func _on_flying_timer_timeout() -> void:
+	print("CanFly true")
+	canFly = true
+	
+	
+func _on_flap_timer_timeout() -> void:
+	flying_bird.icon = BIRD_ONE
+	await get_tree().create_timer(randf_range(0.1,0.4)).timeout
+	flying_bird.icon = BIRD_TWO
+	flap_timer.wait_time = randf_range(0.1,0.4)
+	flap_timer.start()
